8616e88d678748934d1172ae96e9845a45c3cdbe
PaulBrownMagic <pbrownpaul@gmail.com>
2018-02-21 22:03:19 +0000

    Consume Your Data

--- flask_foaf/main.py
+++ flask_foaf/main.py
@@ -1,6 +1,24 @@
 from flask import Flask, render_template
+from rdflib import Graph
+from rdflib.namespace import FOAF
+
 app = Flask(__name__)
+
+foaf = Graph()
+foaf.parse("flask_foaf/static/rdf/foaf.rdf")
+
+
+def get_me():
+    res = foaf.query("""SELECT DISTINCT ?fname
+                     WHERE {
+                     ?me a foaf:Person .
+                     ?me foaf:givenname ?fname .
+                     }
+                     """, initNs={"foaf": FOAF})
+    return list(res)[0]
 
 @app.route("/")
 def home_page():
-    return render_template("index.html")
+    return render_template("index.html",
+                           me= get_me()
+                           )
